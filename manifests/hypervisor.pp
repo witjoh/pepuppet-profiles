a3e34b4043d18684af2517c9244133a0f61df549
Johan De Wit <johan@open-future.be>
2015-01-11 00:07:28 +0100

    initial commit

---
+++ manifests/hypervisor.pp
@@ -0,0 +1,23 @@
+# class profiles::hypervisor
+#
+#  This profiles installs an hypervisor mostly
+#  with default settings.  
+#  The base configuration is kvm
+
+class profiles::hypervisor {
+
+
+  class { '::libvirt':
+    defaultnetwork => true,
+  }
+
+  libvirt_pool { 'guest_images_pool':
+    ensure     => present,
+    type       => 'logical',
+    activate   => true,
+    autostart  => true,
+    sourcedev  => '/dev/sdb1',
+    sourcename => 'guest_images_lvm',
+    target     => '/dev/guest_images_lvm',
+  }
+}
